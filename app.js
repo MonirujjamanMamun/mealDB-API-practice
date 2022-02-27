b17beeb895d5c8a4313716846fda30a8a2bd3a7a
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-02-28 00:16:51 +0600

    food section complited

---
+++ app.js
@@ -0,0 +1,54 @@
+const loadData = () => {
+    const inputText = document.getElementById('input-fild');
+    const inputValue = inputText.value;
+    inputText.value = '';
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayData(data.meals))
+}
+
+
+const displayData = (numbers) => {
+    // console.log(numbers)
+    const foodContainer = document.getElementById("food-container");
+    for (const number of numbers) {
+        const div = document.createElement('div');
+        div.classList.add("col");
+        div.innerHTML = `
+        <div onclick="singelFood('${number.idMeal}')" class="card h-100">
+            <img src="${number.strMealThumb}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h2 class="card-title">${number.strMeal}</h2>
+                <p class="card-text">${number.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+        `;
+        foodContainer.appendChild(div);
+    }
+}
+
+const singelFood = (data) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${data}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => singleFoodDisplay(data.meals[0]))
+}
+
+const singleFoodDisplay = (number) => {
+    console.log(number)
+    const singleFood = document.getElementById("single-food");
+    singleFood.innerHTML = `
+        <div class="card">
+            <img src="${number.strMealThumb}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h1>Meal Id : ${number.idMeal}</h1>
+                <h2>Meal Name : ${number.strMeal}</h2>
+                <h3>Meal Area : ${number.strArea}</h3>
+                <h5>Meal Category : ${number.strCategory}</h5>
+                <p class="card-text">${number.strInstructions}</p>
+            </div>
+        </div>   
+    `;
+}
